c377366466d6bcdf71ac199695eff6a6d68de039
Lucas-TA <dev.ascencao@gmail.com>
2024-04-11 16:37:33 -0300

    finished task

---
+++ Tarefa_Wrapper/src/br/com/lascencao/tarefa_wrapper.java
@@ -0,0 +1,12 @@
+package br.com.lascencao;
+
+public class tarefa_wrapper {
+
+	public static void main(String[] args) {
+		int variable = 27;
+		System.out.print("Tipo primitivo: " + variable + "\n");
+		Integer variableWrapper = variable;
+		System.out.print("Tipo Wrapper: " + variableWrapper + "\n");
+	}
+
+}
